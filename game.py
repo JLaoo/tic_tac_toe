734bf4ba95a919ed775f7af88134043984820891
Jon Luo <jonluo@berkeley.edu>
2018-12-19 20:21:12 -0800

    update 2

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 initial_board = """
 _{}_|_{}_|_{}_
 _{}_|_{}_|_{}_
@@ -19,23 +21,26 @@ winning_boards = [
 [2,4,6]
 ]
 class Game:
-	pos_dict = {0:"_", 1:"_",2:"_", 3:"_",4:"_", 5:"_",6:" ", 7:" ",8:" "}
-	board = initial_board
-	positions = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
 	def __init__(self):
-		print(starting_board)
+		self.pos_dict = {0:"_", 1:"_",2:"_", 3:"_",4:"_", 5:"_",6:" ", 7:" ",8:" "}
+		self.board = initial_board
+		self.positions = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
 		self.who = 0
 
 	def play_turn(self):
 		player = self.who + 1
 		valid_choice = False
 		while not valid_choice:
-			print(f"Player {player}, please enter a board position or type exit to leave")
-			player_choice = int(input())
-			if player_choice >= 0 and player_choice <= 8 and self.positions[player_choice] == -1:
-				valid_choice = True
+			print(f"Player {player}, please enter a board position")
+			try:
+				player_choice = int(input())
+			except ValueError:
+				print("Please enter a number")
 			else:
-				print("Please enter a valid board position")
+				if player_choice >= 0 and player_choice <= 8 and self.positions[player_choice] == -1:
+					valid_choice = True
+				else:
+					print("Please enter a valid board position")
 		self.update(player_choice, self.who, self.pos_dict)
 		self.who = abs(self.who - 1)
 
@@ -45,7 +50,7 @@ class Game:
 			return 3
 		for board in winning_boards:
 			if self.positions[board[0]] == self.positions[board[1]] == self.positions[board[2]] and self.positions[board[0]] != -1:
-				return self.who
+				return abs(self.who - 1)
 		return -1
 
 	def update(self, player_choice, who, pos_dict):
@@ -62,11 +67,45 @@ _{pos_dict[3]}_|_{pos_dict[4]}_|_{pos_dict[5]}_       _3_|_4_|_5_
 		self.positions[player_choice] = who
 
 	def play_game(self):
+		print(starting_board)
 		win = self.win_condition()
 		while win == -1:
 			self.play_turn()
 			win = self.win_condition()
-		if win == 1 or win == 2:
-			print(f"Congratulations Player {win}, you've won!")
+		if win == 0 or win == 1:
+			player = win + 1
+			print(f"Congratulations Player {player}, you've won!")
 		else:
 			print("The game ends in a draw!")
+
+	def easy_AI_turn(self):
+		choice = random.randint(0, 8)
+		while self.positions[choice] != -1:
+			choice = random.randint(0,8)
+		self.update(choice, self.who, self.pos_dict)
+		self.who = abs(self.who - 1)
+
+	def play_vs_AI(self, difficulty):
+		print(starting_board)
+		if difficulty == 1:
+			AI = self.easy_AI_turn
+		if difficulty == 2:
+			AI = self.easy_AI_turn
+		else:
+			AI = self.easy_AI_turn
+		win = self.win_condition()
+		while win == -1:
+			self.play_turn()
+			if self.win_condition() != -1:
+				win = self.win_condition()
+				break
+			AI()
+			win = self.win_condition()
+		if win == 0:
+			print("Congratulations Player, you have won!")
+		elif win == 1:
+			print("Better luck next time :(")
+		else:
+			print("The game ends in a draw!")
+
+
--- game_player.py
+++ game_player.py
@@ -1,4 +1,30 @@
 from game import *
-
-ttt = Game()
-ttt.play_game()
+while True:
+	ttt = Game()
+	print("""
+Choose an option:
+1 - Human vs Human
+2 - Easy AI
+3 - Medium AI
+4 - Hard AI
+5 - Exit
+"""
+	)
+	choice = input()
+	try:
+		choice = int(choice)
+	except ValueError:
+		print("Please enter a number")
+	else:
+		if choice < 1 or choice > 5:
+			print("Please enter a valid option")
+		elif choice == 1:
+			ttt.play_game()
+		if choice == 2:
+			ttt.play_vs_AI(1)
+		elif choice == 3:
+			print("Still in development...")
+		elif choice == 4:
+			print("Still in development...")
+		elif choice == 5:
+			break
